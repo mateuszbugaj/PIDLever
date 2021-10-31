0753c587acc9961ebba26b8425bb817c13cc8038
mateuszbugaj <mateuszbugaj5@gmail.com>
2021-10-31 13:10:03 +0100

    Added .gitignore, deleted unimporatnt files

--- src/PIDLever.c
+++ src/PIDLever.c
@@ -2,6 +2,14 @@
 #include <avr/interrupt.h>
 #include <util/delay.h>
 #include <avr/power.h>
+#include <avr/interrupt.h>
+
+#define PULSE_MIN 1000
+#define PULSE_MAX 2000
+
+void initTimerServo(void){
+    TCCR1A |= (1 << WGM11);
+}
 
 int main(void){
     clock_prescale_set(clock_div_1); // CPU Clock at 8MHz
